da816a2f53a22f03572e2f0cee3170a923529693
Javeria Sheraz <javsheraz1208@gmail.com>
2025-08-28 19:57:41 +0500

    Update screenmouse.py

--- screenmouse.py
+++ screenmouse.py
@@ -1,110 +1,149 @@
-# For this project I have imported a pre-defined repository containing the
-# handDetector class from GitHub named as handTrackingModule.py as htm
-# and made changes to it according to my requirements
-# Most of the methods and functions used in this code are defined in htm file
-
 import cv2
 import numpy as np
-import handTrackingModule as htm
+import mediapipe as mp
+import math
 import time
 import autopy
 import pyautogui
 
-widthCam = 380
-heightCam = 320
+##########FUNCTIONS#########
+def findPosition(image, results, handNo = 0, DRAW = True):
+    lmList = []
+    if results.multi_hand_landmarks:
+        myHand = results.multi_hand_landmarks[handNo]
+        h, w, c = image.shape
+        for id, lm in enumerate(myHand.landmark):
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            lmList.append([id, cx, cy])
+            # UNCOMMENT THSI IF YOU WANT CIRCLES DRAWN ON LANDMARKS OF YOUR HANDS
+            # if DRAW:
+            #     cv2.circle(image, (cx, cy), 4, (255, 0, 0), cv2.FILLED)
+    return lmList
+
+def fingersUp(lmlist):
+    fingers = []
+    tipsIdx = [4, 8, 12, 16, 20]
+    if len(lmlist) == 0:
+        return []
+    #CONDITION TO DETECT THUMB
+    #As the thumb moves sideways on the screen we compare its index of tips (x coordinate [1]) with the previous tips index
+    if lmlist[tipsIdx[0]][1] < lmlist[tipsIdx[0] - 1][1]:
+        fingers.append(1)
+    else:
+        fingers.append(0)
+    # Working: Left a finger(tip) is, the smaller its x-number becomes
+    # CONDITION TO DETECT OTHER FINGERS
+    for id in range(1, 5):
+        if lmlist[tipsIdx[id]][2] < lmlist[tipsIdx[id] - 2][2]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+            # Higher a finger is, the smaller its y-number becomes. This checks the tip and two indexes below it
+    return fingers
+
+def findDistance(lmlist, p1, p2, image, draw=True):
+        x1, y1 = lmlist[p1][1:]
+        x2, y2 = lmlist[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        if draw:
+            # drawing lines to connecting hand landmarks
+            cv2.line(image, (x1, y1), (x2, y2), (230, 230, 230), 2)
+            # drawing circles to map the landmarks on hand
+            cv2.circle(image, (x1, y1), 4, (255, 0, 0), cv2.FILLED)
+            cv2.circle(image, (x2, y2), 4, (255, 0, 0), cv2.FILLED)
+            cv2.circle(image, (cx, cy), 4, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+        return length, image, [x1, y1, x2, y2, cx, cy]
+
+############################
+
+widthCam = 360
+heightCam = 400
 frameReduction = int(widthCam * 0.1)
-smoothen = 8
-ptime = 0
+smoothen = 7
 prev_locx, prev_locy = 0, 0
 cur_locx, cur_locy = 0, 0
 
-cap = cv2.VideoCapture(0)
-cap.set(3,widthCam )
-cap.set(4, heightCam)
-detector = htm.handDetector(maxHands=1)
+cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH,widthCam )
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, heightCam)
 widthScr, heightScr = autopy.screen.size()
-# print(widthScr, heightScr)
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+
 
 while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img)
+    success, img = cam.read()
+    img = cv2.flip(img, 1)
+    if success:
+        convertedImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results= hands.process(convertedImg)
 
     # lmList is the list of landmarks on hands extracted by mediapipe
-    if len(lmList)!=0:
-        # coordinates for index finger, index finger tip =8
-        x1, y1 = lmList[8][1:]
-        # coordinates for thumb finger, thumb finger tip =4
-        x2, y2 =lmList[4][1:]
-        # print(x1, y1, x2, y2)
-        # coordinates for middle finger, middle finger tip =12
-        x4, y4 = lmList[12][1:]
+    lmList = findPosition(img, results)
+    if len(lmList) != 0:
+        # coordinates for thumb finger, thumb finger tip =8
+        x1 = lmList[4][1]
+        y1 = lmList[4][2]
+        # coordinates for index finger, index finger tip =4
+        x2 = lmList[8][1]
+        y2 = lmList[8][2]
+        # coordinates for middle finger, middle fingertip =12
+        x3 = lmList[12][1]
+        y3 = lmList[12][2]
 
-        # to check which fingers are up we call a method of fingersUp from handTrackingModule
-        fingers = detector.fingersUp()
-        # print(fingers)
+        fingers = fingersUp(lmList)
 
-        # fingers[1] represent thumb and fingers [2] represent middle finder
-
-        # MOVING ACTION OF MOUSE CURSOR ON THE SCREEN
-        # For moving the cursor we use index finger, middle and thumb finger = 0 here
-        if fingers[0] == 0 and fingers[1] == 1 and fingers[2] ==0:
-            cv2.rectangle(img,(frameReduction, frameReduction), (widthCam - frameReduction, heightCam - frameReduction), (255, 255, 255), 1)
-            x3 = np.interp(x1, (frameReduction, widthCam - frameReduction), (0, widthScr))
-            y3 = np.interp(y1, (frameReduction, heightCam - frameReduction), (0, heightScr))
-
+        # CURSOR MOVING ACTION OF MOUSE ON THE SCREEN
+        if fingers[1] == 1 and fingers[2] == 0:
+            cv2.rectangle(img, (frameReduction, frameReduction),
+                          (widthCam - frameReduction, heightCam - frameReduction), (255, 255, 255), 1)
+            x4 = np.interp(x2, (frameReduction, widthCam - frameReduction), (0, widthScr))
+            y4 = np.interp(y2, (frameReduction, heightCam - frameReduction), (0, heightScr))
             # smoothening the values of mouse click
-            cur_locx = prev_locx + (x3 - prev_locx)/smoothen
-            cur_locy = prev_locy + (y3 - prev_locy) / smoothen
+            cur_locx = prev_locx + (x4 - prev_locx) / smoothen
+            cur_locy = prev_locy + (y4 - prev_locy) / smoothen
 
-            if abs(cur_locx - prev_locx) < 1.5 and abs(cur_locy - prev_locy) < 1.5:
-                continue  # skip update if finger movement is too small
-
-            autopy.mouse.move(widthScr - cur_locx, cur_locy)
-            cv2.circle(img, (x1, y1), 9, (255, 0, 255), cv2.FILLED)
-            prev_locx, prev_locy = cur_locx, cur_locy
+        if abs(cur_locx - prev_locx) > 1.5 and abs(cur_locy - prev_locy) > 1.5:
+            autopy.mouse.move(cur_locx, cur_locy)
+            cv2.circle(img, (x2, y2), 7, (28, 28, 28), cv2.FILLED)
+            cv2.putText(img, "Moving cursor", (img.shape[1] - 300, img.shape[0] - 13), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (255, 255, 255), 6)
+            cv2.putText(img, "Moving cursor", (img.shape[1] - 300, img.shape[0] - 13), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (0, 0, 0), 2)
+        prev_locx, prev_locy = cur_locx, cur_locy
 
         # CLICKING ACTION OF MOUSE CURSOR ON THE SCREEN
-        #For clicking, we bring together both thumb finger and index finger
+        # For clicking, we bring together both thumb finger and index finger
         if fingers[1] == 1:
-            # calculating straight line between the thumb and index finger
-            rough_dist = np.hypot(x2 - x1, y2 - y1)
-            if rough_dist < 60:
-                length, img, lineinfo = detector.findDistance(8, 4, img, draw=True)
+            rough_dist = np.hypot(x1 - x2, y1 - y2)
+            if rough_dist < 40:
+                length, img, lineinfo = findDistance(lmList, 8, 4, img, draw=True)
+                cv2.putText(img, "Pinch to click", (img.shape[1] - 300, img.shape[0] - 13), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (255, 255, 255), 6)
+                cv2.putText(img, "Pinch to click", (img.shape[1] - 300, img.shape[0] - 13), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (0, 0, 0), 2)
                 if length < 18:
-                    cv2.circle(img, (lineinfo[4], lineinfo[5]), 8, (0, 255, 0), cv2.FILLED)
-                    # green circle indicates that the cursor is clicking is
-                    # Clicking part
+                    cv2.circle(img, (lineinfo[4], lineinfo[5]), 7, (0, 255, 0), cv2.FILLED)
+                    # CLICKING PART (green circle indicates that the cursor is clicking is)
                     autopy.mouse.click()
-                    time.sleep(0.1)
+                    time.sleep(0.2)
 
-
-        # SCROLLING ACTION
+        # SCROLLING DOWN ACTION
         # When both index and middle fingers are up AND close together
-        if fingers[1] == 1 and fingers[2] == 1:
-            length, img, lineinfo = detector.findDistance(8, 12, img)
-
-            if length <15:
-                cv2.circle(img, (lineinfo[4], lineinfo[5]), 8, (0, 255, 0), cv2.FILLED)
-                y_diff = y1 - y4
+        if fingers[0] ==0 and fingers[1] == 1 and fingers[2] == 1:
+            length, img, lineinfo = findDistance(lmList, 8, 12, img)
+            cv2.putText(img, "Put together to scroll", (img.shape[1] - 300, img.shape[0] - 13), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (255, 255, 255), 6)
+            cv2.putText(img, "Put together to scroll", (img.shape[1] - 300, img.shape[0] - 13), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (0, 0, 0), 2)
+            if length < 25:
+                cv2.circle(img, (lineinfo[4], lineinfo[5]), 7, (0, 255, 0), cv2.FILLED)
+                y_diff = y2 - y3
                 if y_diff > 8:
                     pyautogui.scroll(-50)  # scroll down
-                time.sleep(0.1)
+                time.sleep(0.05)
 
-
-    ctime = time.time()
-    fps = 1/(ctime-ptime)
-    ptime = ctime
-
-    # to print frame rate on the screen, uncomment
-    # cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,(255, 0, 0), 3)
-
-    cv2.imshow("Video", img)
+    cv2.imshow("CONTROL YOUR SCREEN CURSOR WITH FINGERS", img)
     key = cv2.waitKey(5)
     # the captured video window closes on pressing the escape key
     if key == 27:
         break
 
-cap.release()
-
+cam.release()
 cv2.destroyAllWindows()
